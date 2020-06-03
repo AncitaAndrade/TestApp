aa29fff53de056d2703604e691a654a9a1fb70d0
Ancita Andrade <ancita.andrade.ext@sivantos.com>
2020-06-03 11:20:11 +0530

    Added UI for project

---
+++ TestApp/TestApp.iOS/CustomButtonRenderer.cs
@@ -0,0 +1,29 @@
+﻿using TestApp.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.Button), typeof(CustomButtonRenderer))]
+
+namespace TestApp.iOS
+{
+    public class CustomButtonRenderer : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+
+            var customButton = e.NewElement as CustomButton;
+
+            var thisButton = Control as UIButton;
+            thisButton.TouchDown += delegate
+            {
+                customButton.OnPressed();
+            };
+            thisButton.TouchUpInside += delegate
+            {
+                customButton.OnReleased();
+            };
+        }
+    }
+}
--- TestApp/TestApp/MainPage.xaml.cs
+++ TestApp/TestApp/MainPage.xaml.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VoiceBasedApp;
+using Xamarin.Essentials;
 using Xamarin.Forms;
+using PermissionStatus = Plugin.Permissions.Abstractions.PermissionStatus;
 
 namespace TestApp
 {
@@ -13,9 +18,82 @@ namespace TestApp
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        private ISpeechToTextService _speechRecongnitionService;
+
         public MainPage()
         {
             InitializeComponent();
+            try
+            {
+                _speechRecongnitionService = DependencyService.Get<ISpeechToTextService>();
+                MyButton.ImageSource = ImageSource.FromResource("TestApp.Images.mic.png");
+                SpeakInitialInstruction();
+            }
+            catch (Exception ex)
+            {
+                recon.Text = ex.Message;
+            }
+
+
+            MessagingCenter.Subscribe<ISpeechToTextService, string>(this, "STT", (sender, args) =>
+            {
+                SpeechToTextFinalResultRecieved(args);
+            });
+
+            MessagingCenter.Subscribe<ISpeechToTextService>(this, "Final", (sender) =>
+            {
+                //used for iOS specific code. Enabling button is done here
+
+            });
+        }
+
+        private void SpeakInitialInstruction()
+        {
+            TextToSpeech.SpeakAsync("To Speak Press and Hold the microphone Image. Release when done!");
+        }
+
+        private void SpeechToTextFinalResultRecieved(string args)
+        {
+            recon.Text = args;
+        }
+
+        private async Task<bool> GetPermissionStatusAsync()
+        {
+            var permissionStatus = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Microphone);
+            if (PermissionStatus.Granted == permissionStatus)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private async void MyButton_Pressed(object sender, EventArgs e)
+        {
+            var canProceed = await GetPermissionStatusAsync();
+
+            if (canProceed)
+            {
+                MyButton.ImageSource = ImageSource.FromResource("TestApp.Images.MicrophoneOnMute.png");
+                _speechRecongnitionService.StartSpeechToText();
+
+            }
+            else
+            {
+                if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.Microphone))
+                {
+                    await DisplayAlert("Need Mic", "Need Microphone Access", "OK");
+                }
+
+                var status = await CrossPermissions.Current.RequestPermissionAsync<MicrophonePermission>();
+                if (status == PermissionStatus.Granted)
+                    MyButton_Pressed(sender, e);
+            }
+        }
+
+        private void MyButton_Released(object sender, EventArgs e)
+        {
+            MyButton.ImageSource = ImageSource.FromResource("TestApp.Images.mic.png");
+            _speechRecongnitionService.StopSpeechToText();
         }
     }
 }
